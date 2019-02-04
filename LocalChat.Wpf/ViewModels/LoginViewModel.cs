850fbd28336753e03138fe792b84a855d8ba2e35
Unknown <kaledaviktoriya@gmail.com>
2019-02-04 11:21:40 +0300

    Add WPF application realization.

---
+++ LocalChat.Wpf/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using DevExpress.Mvvm;
+using LocalChat.Wpf.Validators;
+using System.Globalization;
+using System.Windows.Input;
+
+namespace LocalChat.Wpf.ViewModels
+{
+    public class LoginViewModel : BindableBase
+    {
+        public string Username
+        {
+            get { return GetProperty(() => Username); }
+            set { SetProperty(() => Username, value); }               
+        }
+    }
+}
---
+++ LocalChat.Wpf/ViewModels/MainViewModel.cs
@@ -0,0 +1,66 @@
+﻿using DevExpress.Mvvm;
+using LocalChat.Service;
+using LocalChat.Wpf.ViewModels;
+using LocalChat.Wpf.Views;
+using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Windows.Input;
+
+namespace LocalChat.Wpf
+{
+    public class MainViewModel : BindableBase
+    {
+        public ObservableCollection<Message> Messages { get; } = new ObservableCollection<Message>();
+
+        public string CurrentMessage
+        {
+            get { return GetProperty(() => CurrentMessage); }
+            set { SetProperty(() => CurrentMessage, value); }
+        }
+
+        private LocalChatClient _localChat;
+        private Thread _listeningThread;
+
+        public ICommand WindowLoadedCommand
+            => new DelegateCommand(async () =>
+            {
+                var view = new LoginDialog
+                {
+                    DataContext = new LoginViewModel()
+                };
+
+                var username = (string) await DialogHost.Show(view, "RootDialog");
+
+                _localChat = new LocalChatClient(username);
+
+                _localChat.SendSystemMessage($"{username} joined the chat.");
+
+                _localChat.MessageReceive += OnMessageReceived;
+
+                _listeningThread = new Thread(new ThreadStart(_localChat.Listen));
+                _listeningThread.Start();
+            });
+
+        public ICommand SendMessage
+            => new DelegateCommand(() =>
+            {
+                if (string.IsNullOrEmpty(CurrentMessage) || string.IsNullOrWhiteSpace(CurrentMessage))
+                    return;
+
+                _localChat.SendUserMessage(CurrentMessage);
+
+                CurrentMessage = "";
+            });
+
+        public ICommand WindowClosingCommand
+            => new DelegateCommand(() => Environment.Exit(0));
+
+        public void OnMessageReceived(object sender, MessageReceiveEventArgs messageReceiveEventArgs)
+        {
+            System.Windows.Application.Current.Dispatcher
+                .Invoke(() => Messages.Add(messageReceiveEventArgs.Message));            
+        }
+    }
+}
